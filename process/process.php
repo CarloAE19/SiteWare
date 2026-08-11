cd2e762abfab9a77f8cc4563f4860a5eba5c8555
CarloAE19 <carlopedrosa14@gmail.com>
2026-08-11 08:03:53 +0800

    feat: implement central request router and SMS gateway integration for purchasing workflows

--- process/process.php
+++ process/process.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         } elseif (in_array($action, ['add_user', 'edit_user', 'delete_user', 'update_profile', 'verify_current_password', 'change_password_modal'])) {
             require __DIR__ . '/module_users.php';
         }
-        elseif (in_array($action, ['create_rs', 'approve_rs', 'reject_rs', 'stage_rs_materials', 'create_po', 'update_po_eta', 'mark_po_delivered', 'send_po_sms', 'log_po_delay', 'create_withdrawal', 'fetch_rs_data', 'fetch_rs_with_history', 'fetch_po_items', 'fetch_po_details', 'fetch_supplier_delivery_history', 'fetch_po_sms_preview', 'fetch_sms_threads', 'fetch_sms_messages', 'send_supplier_reply_sms', 'mark_sms_thread_read', 'fetch_combined_alerts'])) {
+        elseif (in_array($action, ['create_rs', 'approve_rs', 'reject_rs', 'stage_rs_materials', 'create_po', 'update_po_eta', 'mark_po_delivered', 'send_po_sms', 'log_viber_order_sent', 'log_po_delay', 'create_withdrawal', 'fetch_rs_data', 'fetch_rs_with_history', 'fetch_po_items', 'fetch_po_details', 'fetch_supplier_delivery_history', 'fetch_po_sms_preview', 'fetch_sms_threads', 'fetch_sms_messages', 'send_supplier_reply_sms', 'mark_sms_thread_read', 'fetch_combined_alerts'])) {
             require __DIR__ . '/module_transactions.php';
         } elseif ($action === 'submit_audit') {
             require __DIR__ . '/module_audit.php';
